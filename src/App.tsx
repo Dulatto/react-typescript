95639ced5ac1bd9ad9870069f010ab7fce883a58
Dulatto <dulat.omarov@gmail.com>
2022-01-30 20:20:53 -0500

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Card, {CardVariant} from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
@@ -7,7 +8,19 @@ const App = () => {
 
   const [users, setUsers] = useState<IUser[]>([]);
 
-  
+  useEffect(() => {
+     fetchUsers()
+
+  }, [])
+
+  async function fetchUsers(){
+    try{
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch(e){
+      alert(e)
+    }
+  }
 
   return (
     <div>
