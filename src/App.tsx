8a7ac5751b846173dce8abe63158c77a5382321f
Dulatto <dulat.omarov@gmail.com>
2022-01-31 21:57:16 -0500

    fetch todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card, {CardVariant} from './components/Card';
 import List from './components/List';
+import TodoItem from './components/TodoItem';
 import UserItem from './components/UserItem';
 import UserList from './components/UserList';
 import { ITodo, IUser } from './types/types';
@@ -40,6 +41,8 @@ const App = () => {
        <button>Button</button>
      </Card>
      <List items={users} renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>} 
+     />
+      <List items={todos} renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>} 
      />
     </div>
   );
