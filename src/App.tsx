dc6551a129a60a0bb26a3c30f26acabdda980dc2
Dulatto <dulat.omarov@gmail.com>
2022-02-02 22:47:58 -0500

    users page

--- src/App.tsx
+++ src/App.tsx
@@ -11,41 +11,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
 
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [todos, setTodos] = useState<ITodo[]>([]);
 
-  useEffect(() => {
-     fetchUsers()
-     fetchTodos()
-  }, [])
-
-  async function fetchUsers(){
-    try{
-      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-      setUsers(response.data)
-    } catch(e){
-      alert(e)
-    }
-  }
-  
-  async function fetchTodos(){
-    try{
-      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      setTodos(response.data)
-    } catch(e){
-      alert(e)
-    }
-  }
 
   return (
     <div>
-      <EventsExample />
-     <Card onClick={(num)=> console.log('click', num)} variant={CardVariant.primary} width="200px" height="200px">
-       <button>Button</button>
-     </Card>
     
-      <List items={todos} renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>} 
-     />
+    
     </div>
   );
 };
